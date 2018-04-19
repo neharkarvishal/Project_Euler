caff18c61cfdf613f2daf0e927e268538cb01ae6
Vishal Neharkar <37950421+neharkarvishal@users.noreply.github.com>
2018-04-20 00:16:07 +0530

    Update problem003.java

--- Java/problem003.java
+++ Java/problem003.java
@@ -1,1 +1,26 @@
+/* * Solution to Project Euler problem 3 in Java */
 
+package project.euler.java;
+
+public class problem003 
+{
+    public static void main(String[] args)
+    {
+        long N = 600851475143L;
+        
+        while(N % 2 == 0)
+        {
+            N = N/2;
+        }
+        
+        for(long factor = 3; factor < N; factor = factor + 2)
+        {
+            while(N % factor == 0 && factor < N)
+            {
+                N = N / factor;
+            }
+        }
+        System.out.println(N);
+    }
+
+}
