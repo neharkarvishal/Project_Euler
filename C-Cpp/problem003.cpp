d15399a7f5e26b5cd6a96359ebd5bfca65c7da5e
Vishal Neharkar <37950421+neharkarvishal@users.noreply.github.com>
2018-04-20 00:25:45 +0530

    Update problem003.cpp

--- C-Cpp/problem003.cpp
+++ C-Cpp/problem003.cpp
@@ -1,1 +1,24 @@
+/* * Solution to Project Euler problem 3 C++ */
+#include <cstdlib> 
+#include "iostream"  // less than and greater than are disabled
 
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+        long N = 600851475143L;
+        
+        while(N % 2 == 0)
+        {
+            N = N/2;
+        }
+        
+        for(long factor = 3; factor < N; factor = factor + 2)
+        {
+            while(N % factor == 0 && factor < N)
+            {
+                N = N / factor;
+            }
+        }
+        cout<<N;
+}
